4111245959bb1f0cbbd6dbd14d560e69130ed904
AITI GenZ <alwyfasa88@gmail.com>
2026-06-13 10:52:34 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/filament/admin/theme.css',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/admin/theme.css",
             ],
             refresh: true,
+            phpBinary: "C:/laragon/bin/php/php-8.3.30-Win32-vs16-x64/php.exe",
         }),
     ],
 });
